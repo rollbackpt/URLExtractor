39312878e4930de5c63a3b6659f5ca99cda2bbcf
rollbackpt <joaopedrocr@gmail.com>
2015-10-04 22:54:41 +0100

    Fixes in the demo

--- Demo/urlinfo.php
+++ Demo/urlinfo.php
@@ -2,13 +2,12 @@
 
 require_once '../URLExtractor/urlextractor.class.php';
 
-
 if (isset($_POST['url']) && !empty($_POST['url'])) {
 
 	$urlEmbed = new UrlExtractor($_POST['url']);
 	echo $urlEmbed->extractAll();
-	
-	/*
+
+/*
 	echo 'title -> <pre>';
 	var_dump($urlEmbed->title);
 	echo '</pre><br/><br/>description -> <pre>';
@@ -18,5 +17,5 @@ if (isset($_POST['url']) && !empty($_POST['url'])) {
 	echo '</pre><br/><br/>images -> <pre>';
 	var_dump($urlEmbed->images);
 	echo '</pre><br/>';
-	*/
+*/
 }
